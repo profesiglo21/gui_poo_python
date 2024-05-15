b5455d91b73237289ca5a6901d19aeaf2b65889e
Salim Tieb Mohamedi <profesiglo21@gmail.com>
2024-05-15 19:48:16 +0200

    Add files via upload
    añadir sentencias de creación

--- coding/mysql/create_alter_drop_tables.py
+++ coding/mysql/create_alter_drop_tables.py
@@ -10,6 +10,42 @@ try:
         password=getpass("Enter password: "),
         database="online_movie_rating"
     ) as connection:
-       print(connection)
+        create_movies_table_query = """
+        CREATE TABLE movies(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(100),
+        release_year YEAR(4),
+        genre VARCHAR(100),
+        collection_in_mil INT
+        )
+        """
+
+        create_reviewers_table_query = """
+        CREATE TABLE reviewers (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(100),
+        last_name VARCHAR(100)
+        )
+        """
+
+        create_ratings_table_query = """
+        CREATE TABLE ratings (
+        movie_id INT,
+        reviewer_id INT,
+        rating DECIMAL(2,1),
+        FOREIGN KEY(movie_id) REFERENCES movies(id),
+        FOREIGN KEY(reviewer_id) REFERENCES reviewers(id),
+        PRIMARY KEY(movie_id, reviewer_id)
+        )
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(create_movies_table_query)
+            cursor.execute(create_reviewers_table_query)
+            cursor.execute(create_ratings_table_query)
+            connection.commit()
+
+
+
 except Error as e:
     print(e)
